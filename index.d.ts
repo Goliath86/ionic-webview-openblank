a1b46c90a510ff38e0ec48552765909403414636
Mattia Mazzon - Evomatic Srl <MattiaM@evomatic.local>
2020-01-17 14:18:03 +0100

    First commit

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import { IonicNativePlugin } from '@ionic-native/core';
+/**
+ * @name IonOpenBlank
+ * @description
+ * Access the function of the GooglePay API on Android devices
+ *
+ * @usage
+ * ```typescript
+ * import { IonOpenBlank } from 'ionic-webview-openblank';
+ *
+ * constructor(private ionOpenBlank: IonOpenBlank) { }
+ *
+ * ...
+ *
+ * ```
+ */
+export declare class IonOpenBlank extends IonicNativePlugin {
+}
